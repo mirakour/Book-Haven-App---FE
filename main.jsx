e97bc33f7a98a9a343bc6068da2c91c8f08c6a14
kourtney_miranda <mirandakourt@gmail.com>
2025-06-26 20:41:59 -0700

    Fix Netlify build config

--- main.jsx
+++ main.jsx
@@ -1,32 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import Books from "./pages/Books.jsx";
-import BookDetails from "./pages/BookDetails.jsx";
-import Account from "./pages/Account.jsx";
-import Register from "./pages/Register.jsx";
-import Login from "./pages/Login.jsx";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/*" element={<App />}>
-        <Route index element={<Books />} />
-        <Route path="books" element={<Books />} />
-        <Route path="books/:id" element={<BookDetails />} />
-        <Route
-          path="account"
-          element={
-            <ProtectedRoute>
-              <Account />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
